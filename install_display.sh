77ca4c1f135679129ea8c43cf59adcb483d7fe40
Senific <liveheartstudio@gmail.com>
2025-05-29 05:20:30 +0530

    add logging

--- install_display.sh
+++ install_display.sh
@@ -12,14 +12,14 @@ echo "=== Installing required packages ==="
 sudo apt install -y git fbi
 
 echo "=== Cloning LCD driver repository to home directory ==="
-cd /home/Admin
-if [ -d "/home/Admin/LCD-show" ]; then
+cd /home/admin
+if [ -d "/home/admin/LCD-show" ]; then
     echo "LCD-show already exists, removing..."
-    rm -rf "/home/Admin/LCD-show"
+    rm -rf "/home/admin/LCD-show"
 fi
 
-git clone https://github.com/goodtft/LCD-show.git /home/Admin/LCD-show
-cd /home/Admin/LCD-show
+git clone https://github.com/goodtft/LCD-show.git /home/admin/LCD-show
+cd /home/admin/LCD-show
 chmod +x LCD35-show
 
 echo "=== Installing ILI9486 driver (will reboot) ==="
