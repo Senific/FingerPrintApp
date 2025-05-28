ca3eb865e63340829939553c60492285851f2a57
Senific <liveheartstudio@gmail.com>
2025-05-28 10:03:37 +0530

    test layout

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import os
-os.environ["KIVY_BCM_DISPMANX_DISPLAY"] = "1"
-os.environ["KIVY_WINDOW"] = "sdl2"
-os.environ["SDL_FBDEV"] = "/dev/fb1"
-os.environ["KIVY_GL_BACKEND"] = "gl""
-
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
