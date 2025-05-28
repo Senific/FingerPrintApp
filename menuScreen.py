aa6e05af783a52c4b188e11c9162d44f83536bce
Senific <liveheartstudio@gmail.com>
2025-05-28 23:40:47 +0530

    Updates to the App

--- menuScreen.py
+++ menuScreen.py
@@ -15,7 +15,7 @@ class MenuScreen(Screen):
         layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
 
         background = Image(
-            source="assets/bg2.jpg",
+            source="assets/bg3.jpg",
             allow_stretch=True,
             keep_ratio=False,
             size_hint=(1, 1),
