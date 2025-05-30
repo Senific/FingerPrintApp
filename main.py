be6f81fc5e48129b8504d56971487b8d1e6584ad
Senific <liveheartstudio@gmail.com>
2025-05-30 22:57:43 +0530

    add logging

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # import logging
 # logging.basicConfig(filename='app.log', level=logging.DEBUG)
+import os
+os.environ["KIVY_BCM_DISPMANX_ID"] = "2"
+os.environ["KIVY_METRICS_DENSITY"] = "2"
+os.environ["KIVY_NO_CONSOLELOG"] = "1"
+
 
 from kivy.config import Config
-Config.set('graphics', 'fullscreen', '1')  # or 'auto'
-Config.set('graphics', 'show_cursor', '0')
-Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'width', '320')    # Adjust to your 3.5" screen size
-Config.set('graphics', 'height', '480')
+# Config.set('graphics', 'fullscreen', '1')  # or 'auto'
+# Config.set('graphics', 'show_cursor', '0')
+# Config.set('graphics', 'resizable', '0')
+# Config.set('graphics', 'width', '320')    # Adjust to your 3.5" screen size
+# Config.set('graphics', 'height', '480')
 
 from kivy.core.window import Window
 from idleScreen import IdleScreen
