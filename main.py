47c0b82c739eddb81b1759b97ba29e4a73bafa6d
Senific <liveheartstudio@gmail.com>
2025-05-30 23:17:17 +0530

    add logging

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # import logging
 # logging.basicConfig(filename='app.log', level=logging.DEBUG)
 import os
+os.environ["KIVY_NO_CONSOLELOG"] = "1"
+os.environ["KIVY_NO_FILELOG"] = "1"
 os.environ["KIVY_BCM_DISPMANX_ID"] = "2"
 os.environ["KIVY_METRICS_DENSITY"] = "2"
-os.environ["KIVY_NO_CONSOLELOG"] = "1"
+os.environ["KIVY_CLOCK"] = "default"
+os.environ["KIVY_VIDEO"] = "ffpyplayer"
+os.environ["KIVY_TEXT"] = "pil"
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+os.environ["KIVY_WINDOW"] = "sdl"
+
 
 
 from kivy.config import Config
