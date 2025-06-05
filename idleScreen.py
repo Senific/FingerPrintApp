3cf22419d7273c52e6f87cd31f2ad19860ddca3e
Senific <liveheartstudio@gmail.com>
2025-06-05 22:30:00 +0530

    Add battery level

--- idleScreen.py
+++ idleScreen.py
@@ -17,11 +17,10 @@ if is_raspberry_pi:
     import board
     import busio
     from adafruit_ads1x15.ads1115 import ADS1115
-    from adafruit_ads1x15.analog_in import AnalogIn
-    from adafruit_ads1x15.ads1x15 import ADS1X15   
+    from adafruit_ads1x15.analog_in import AnalogIn 
     i2c = busio.I2C(board.SCL, board.SDA)
     ads = ADS1115(i2c)
-    adc_channel = AnalogIn(ads, ADS1X15.P0  )
+    adc_channel = AnalogIn(ads, ADS1115.P0)  # ✅ Correct in new versions
 else:
     print("Running on Windows: skipping ADS1115 setup.")
     class DummyChannel:
