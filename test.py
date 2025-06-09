95a260ddfa3e4534ea3a94b09c71e0bb6c53ed9d
Senific <liveheartstudio@gmail.com>
2025-06-09 17:41:29 +0530

    GT521f52 test

--- test.py
+++ test.py
@@ -1,8 +1,29 @@
 import serial
+import time
 
+# Open serial port
 ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
 
-ser.write(b'\x55\xAA...')  # Example command to fingerprint module
+# === Build Open command manually ===
+# Header: 55 AA
+# Device ID: 01 00
+# Param: 00 00 00 00
+# Cmd: 01 00
+# Checksum: 01 00 + 00 + 00 + 00 + 00 = 01 00
 
-response = ser.read(32)
-print(response)
+open_command = b'\x55\xAA\x01\x00\x00\x00\x00\x00\x01\x00\x01\x01'
+
+# Send the command
+ser.write(open_command)
+
+# Wait a bit to let the scanner respond
+time.sleep(0.2)
+
+# Read response (usually 12 bytes for ACK/NACK)
+response = ser.read(12)
+
+# Print response in hex
+print("Response:", response.hex())
+
+# Close serial port
+ser.close()
