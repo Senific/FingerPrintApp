586f9604b7a03646c439f80bb32812a8eed3fb3c
Senific <liveheartstudio@gmail.com>
2025-06-21 13:50:53 +0530

    6/21/2025

--- test.py
+++ test.py
@@ -158,7 +158,7 @@ async def main():
         # 7. settemplate - set a template data to device
         if task == 7:
             idx = int(input("Enter ID: "))
-            DATA = test_template_data_14  #[] # a 502 length python list, that we get after running "task 3"
+            DATA = test_template_data  #[] # a 502 length python list, that we get after running "task 3"
             fp.delete(idx=idx)
             status = fp.setTemplate(idx=idx, data=DATA)
             print("\n |__ set template status :", status)
